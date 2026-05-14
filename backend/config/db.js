011dd1a353cf87013cb873e0fb53f0993e4721b9
Dinesh3072002 <dineshshanmugam3072002@gmail.com>
2026-05-14 14:31:00 +0530

    main

--- backend/config/db.js
+++ backend/config/db.js
@@ -9,7 +9,8 @@ const pool = mysql.createPool({
     database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+    ssl: process.env.DB_HOST === 'localhost' ? false : { rejectUnauthorized: false }
 });
 
 module.exports = pool;
